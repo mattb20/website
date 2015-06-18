ff6ed757c34f631c9f3bf27f6e958ce429db4fcc
Leo Allen <leo.f.allen@gmail.com>
2015-06-18 18:31:18 +0100

    Updates navigation.js

--- source/javascripts/navigation.js
+++ source/javascripts/navigation.js
@@ -1,33 +1,36 @@
-//For showing and hiding navigation
-
-var hideNav = function($nav) {
-  // only affect desktop navigation
+(function(){
   var $nav = $('header.navigation');
-  $nav.addClass('hidden-nav');
-};
 
-$(document).ready(function() {
-  // only affect desktop navigation
-  var $nav = $('header.navigation');
-  // hide after 5 seconds
-  var t = window.setTimeout(hideNav, 5000);
+  //For showing and hiding navigation
+  var hideNav = function() {
+    // only affect desktop navigation
+    $nav.addClass('hidden-nav');
+  };
 
-  $nav.hover(
-    function() { clearTimeout(t) },
-    function() { t = window.setTimeout(hideNav, 5000) }
-  );
+  $(document).ready(function() {
+    // only affect desktop navigation
+    // hide after 5 seconds
+    var timeOut = window.setTimeout(hideNav, 5000);
 
-  var lastScrollTop = 0;
-  $(window).scroll(function(e) {
-    var scrollTop = $(this).scrollTop();
-    if (scrollTop > lastScrollTop) {
-      // on scroll down, hide nav
-      $nav.addClass('hidden-nav');
-    } else {
-      // on scroll up, show nav
-      $nav.removeClass('hidden-nav');
-    };
-    lastScrollTop = scrollTop;
+    $nav.hover(
+      function() { clearTimeout(timeOut); },
+      function() { timeOut = window.setTimeout(hideNav, 5000); }
+    );
+
+    var lastScrollTop = 0;
+
+    $(window).scroll(function(e) {
+      var scrollTop = $(this).scrollTop();
+
+      if (scrollTop > lastScrollTop) {
+        // on scroll down, hide nav
+        $nav.addClass('hidden-nav');
+      } else {
+        // on scroll up, show nav
+        $nav.removeClass('hidden-nav');
+      }
+
+      lastScrollTop = scrollTop;
+    });
   });
-
-});
+}());
