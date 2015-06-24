c42616cb8147ef56eb0c991c31e53a465c6aa975
Spike Nox <spike01@gmail.com>
2015-06-24 11:53:33 +0100

    * spec/partner_logos_helper_spec.rb: Removes magic regex variable

--- spec/partner_logos_helper_spec.rb
+++ spec/partner_logos_helper_spec.rb
@@ -5,13 +5,9 @@ class PartnerLogosHelperWrapper
 end
 
 describe PartnerLogosHelperWrapper do
-  let(:test_logo) { "test-logo.png" }
   it "fetches a list of available partner logos" do
+    test_logo = "test-logo.png"
     expect(subject.fetch_logos).not_to be_empty
-  end
-
-  it "outputs image tags for each partner logo" do
-    expect(subject.output_logo_tags([test_logo])[0]).to eq "<img src='hiring-partners/test-logo.png' />"
   end
 
   it "outputs human friendly titles for alt text and title" do
