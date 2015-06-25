d690353172f0c5cd192de1b7ff4f33bc60a39660
Sam Morgan <s_morgan@me.com>
2015-06-25 19:13:02 +0100

    updated YAML with first and last names using Sublime like a boss, and updated the graduate partials. Also specced up a helper for possessivizing names in case they get tricky

---
+++ spec/possessive_helper_spec.rb
@@ -0,0 +1,17 @@
+require 'possessive_helper'
+
+class PossessiveHelperWrapper
+  include PossessiveHelper
+end
+
+describe PossessiveHelperWrapper do
+  xit "possessivizes a regular name" do
+  end
+
+  xit "possessivizes a name that ends in an s" do
+  end
+
+  # And the Leo test
+  xit "possessivizes Leo's name" do
+  end
+end
