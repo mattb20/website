7fdf9f1e83090102eb57d37fefa0b3cd54e1f624
Leo Allen <leo.f.allen@gmail.com>
2015-07-10 11:19:30 +0100

    Adds some more info about using javascript with bower

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,5 +1,10 @@
-// All, loaded in the footer
+// All javascripts loaded in the footer
 
+// Note the dependencies listed below will only be loaded in if the asset
+// pipeline is set up. If you're loading this using Bower you'll need to include
+// these yourself - the simplest way is just to add them using <script> tags in
+// your layout html file
+//
 //= require vendor/jquery
 //= require vendor/retina.min
 //= require vendor/slick.js
