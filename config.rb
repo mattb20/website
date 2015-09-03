8ead9cd5f5367232f6a55ffc44cec50415c7273a
Arfah <arfahfarooq@gmail.com>
2015-09-03 15:49:10 +0100

    Added 4 new case studies, added back denise and mixed up order
    Closes #367

--- config.rb
+++ config.rb
@@ -1,7 +1,7 @@
 activate :dotenv
 # This is the website version registered in Mixpanel
 # It should match the minor version (e.g. 2.1) of the website
-set :website_version, 2.2
+set :website_version, 2.3
 
 Dir["lib/*.rb"].each { |file| require file }
 
