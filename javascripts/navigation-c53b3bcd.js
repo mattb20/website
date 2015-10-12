286557f80f69cf4b1ca55e08d097311e5b39c72d
Codeship CI <dev@makersacademy.com>
2015-10-12 15:26:57 +0000

    Site updated to b7d5972
    --skip-ci

--- javascripts/navigation-c53b3bcd.js
+++
@@ -1,1 +0,0 @@
-!function(){var n=$("header.navigation"),o=function(){n.addClass("hidden-nav")};$(document).ready(function(){var e=window.setTimeout(o,5e3);n.hover(function(){clearTimeout(e)},function(){e=window.setTimeout(o,5e3)});var i=0;$(window).scroll(function(){var o=$(this).scrollTop();o>0&&o>i?n.addClass("hidden-nav"):n.removeClass("hidden-nav"),i=o})})}();
