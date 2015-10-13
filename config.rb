b2ca6a6fc87f871abb9c4311bddba0a361b75ef2
Stefan Dorunga <stefandorunga@gmail.com>
2015-10-13 11:04:56 +0100

    Extracts graduate numbers into stats

--- config.rb
+++ config.rb
@@ -45,7 +45,15 @@ activate :search_engine_sitemap
 # activate :automatic_image_sizes
 
 # Methods defined in the helpers block are available in templates
-helpers CurrentPageHelper, PartnerLogosHelper, MarkdownHelper, PossessiveHelper, SlugHelper, ImageHelper, GraduatesHelper, RawHelper
+helpers CurrentPageHelper,
+        PartnerLogosHelper,
+        MarkdownHelper,
+        PossessiveHelper,
+        SlugHelper,
+        ImageHelper,
+        GraduatesHelper,
+        RawHelper,
+        StatsHelper
 
 # Proxy pages (https://middlemanapp.com/advanced/dynamic_pages/)
 data.graduates.each do | grad |
