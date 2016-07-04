f4b0b8cb65a90671ca596f02811d56d2bf6771ee
Nikesh Ashar <nikeshashar@gmail.com>
2016-07-04 18:32:02 +0100

    Testing staging

--- source/javascripts/doorbell.js
+++ source/javascripts/doorbell.js
@@ -5,7 +5,7 @@
 //   var g = d.createElement(t);g.id = 'doorbellScript';g.type = 'text/javascript';g.async = true;g.src = 'https://embed.doorbell.io/button/1652?t='+(new Date().getTime());(d.getElementsByTagName('head')[0]||d.getElementsByTagName('body')[0]).appendChild(g);
 // }(document, 'script'));
 
-
+// testing intercom 
 window.intercomSettings = {
     app_id: 'sflt2hlu'
 };
