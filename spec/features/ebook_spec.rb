7970df438600a72b15421a3f3c64bb7ca4955f1c
Peter Miklos <pt.miklos@gmail.com>
2017-06-08 16:38:35 +0100

    add ids to some tags in ebook view, add static content related test cases into ebook_spec.rb

--- spec/features/ebook_spec.rb
+++ spec/features/ebook_spec.rb
@@ -5,10 +5,27 @@ feature "ebook", :type => :feature do
     visit '/ebook'
   end
 
-  scenario "has the correct title header" do
-    expect(page).to have_selector 'h1'
-    within 'h1' do
-      expect(page).to have_content "Learn Ruby In 15 Minutes"
+  describe "static content" do
+    scenario "has the correct title header" do
+      expect(page).to have_selector 'h1'
+      within 'h1' do
+        expect(page).to have_content "Learn Ruby In 15 Minutes"
+      end
+    end
+
+    scenario "has the correct subheader" do
+      expect(page).to have_css("h4#ebook-subheader", :text => "Download the guide")
+    end
+
+    scenario "has the correct intro" do
+      content = "Get a free copy of our Learn Ruby In 15 Minutes e-book by signing "\
+                "up to our newsletter. It's perfect for anyone that is interested "\
+                "in taking the plunge by learning to code. It is a good start into "\
+                "the Ruby language if you've never had any experience before and "\
+                "we've made it as easy to understand as possible."
+
+      expect(page).to have_css("p#ebook-intro", :text => content)
     end
   end
+
 end
