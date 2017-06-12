ff66e6bb6247629e70419fbbb07455740b0843cb
Peter Miklos <pt.miklos@gmail.com>
2017-06-12 14:47:13 +0100

    add more refactoring to quiz-spec.js file

--- spec/javascript/unit/quiz-spec.js
+++ spec/javascript/unit/quiz-spec.js
@@ -20,50 +20,53 @@ describe("quiz", function() {
     $(".quiz-row").quiz();
   });
 
-  it("renders the question properly", function() {
-    var renderedQuestion = $("#quiz-question").text();
-    var question = "If you enter this series of commands into a computer, what " +
-                   "would you expect the output to be.a = 10b = 20a = b";
-    expect(renderedQuestion).toEqual(question);
+  describe("setup", function() {
+    it("renders the question properly", function() {
+      var renderedQuestion = $("#quiz-question").text();
+      var question = "If you enter this series of commands into a computer, what " +
+                     "would you expect the output to be.a = 10b = 20a = b";
+      expect(renderedQuestion).toEqual(question);
+    })
+
+    it("renders the first label properly", function() {
+      var label1 = $("#quiz-answer-label-1").text();
+      expect(label1).toEqual("a = ");
+    })
+
+    it("renders the second label properly", function() {
+      var label2 = $("#quiz-answer-label-2").text();
+      expect(label2).toEqual("b = ");
+    })
   })
 
-  it("renders the first label properly", function() {
-    var label1 = $("#quiz-answer-label-1").text();
-    expect(label1).toEqual("a = ");
-  })
+  describe("evaluation", function() {
+    it("returns the proper respond if correct values are submitted", function() {
+      $("#quiz-answer-input-1").val(20);
+      $("#quiz-answer-input-2").val(20);
+      $("#quiz-submit").trigger("click");
+      var renderedRespond = $("#quiz-result").text();
+      var respond = "Your answer is correct!";
+      expect(renderedRespond).toEqual(respond);
+    })
 
-  it("renders the second label properly", function() {
-    var label2 = $("#quiz-answer-label-2").text();
-    expect(label2).toEqual("b = ");
-  })
+    it("returns the proper respond if incorrect values are submitted", function() {
+      $("#quiz-answer-input-1").val(18);
+      $("#quiz-answer-input-2").val(20);
+      $("#quiz-submit").trigger("click");
+      var renderedRespond = $("#quiz-result").text();
+      var respond = "Your answer: a = 18, b = 20The correct answer is: a = 20, " +
+                    "b = 20Think of this in the same way a computer would. Or even" +
+                    " better, try it out in IRB";
+      expect(renderedRespond).toEqual(respond);
+    })
 
-  it("returns the proper respond if correct values are submitted", function() {
-    $("#quiz-answer-input-1").val(20);
-    $("#quiz-answer-input-2").val(20);
-    $("#quiz-submit").trigger("click");
-    var renderedRespond = $("#quiz-result").text();
-    var respond = "Your answer is correct!";
-    expect(renderedRespond).toEqual(respond);
+    it("removes whitespace from both ends of the input values", function() {
+      $("#quiz-answer-input-1").val("      20  ");
+      $("#quiz-answer-input-2").val("  20       ");
+      $("#quiz-submit").trigger("click");
+      var renderedRespond = $("#quiz-result").text();
+      var respond = "Your answer is correct!";
+      expect(renderedRespond).toEqual(respond);
+    })
   })
-
-  it("returns the proper respond if incorrect values are submitted", function() {
-    $("#quiz-answer-input-1").val(18);
-    $("#quiz-answer-input-2").val(20);
-    $("#quiz-submit").trigger("click");
-    var renderedRespond = $("#quiz-result").text();
-    var respond = "Your answer: a = 18, b = 20The correct answer is: a = 20, " +
-                  "b = 20Think of this in the same way a computer would. Or even" +
-                  " better, try it out in IRB";
-    expect(renderedRespond).toEqual(respond);
-  })
-
-  it("removes whitespace from both ends of the input values", function() {
-    $("#quiz-answer-input-1").val("      20  ");
-    $("#quiz-answer-input-2").val("  20       ");
-    $("#quiz-submit").trigger("click");
-    var renderedRespond = $("#quiz-result").text();
-    var respond = "Your answer is correct!";
-    expect(renderedRespond).toEqual(respond);
-  })
-
 })
