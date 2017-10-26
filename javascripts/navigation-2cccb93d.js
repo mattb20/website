4fb5ea75c9ec07c24d5b5504bb51a0e4fa02688f
Codeship CI <dev@makersacademy.com>
2017-10-26 11:02:27 +0000

    Site updated to 2ee3d86
    --skip-ci

--- javascripts/navigation-2cccb93d.js
+++
@@ -1,1 +0,0 @@
-!function(n,e,a){var o=n("header.navigation.auto-hide"),i=function(){o.addClass("hidden-nav")};n(a).ready(function(){var a=e.setTimeout(i,8e3);o.hover(function(){clearTimeout(a)},function(){a=e.setTimeout(i,8e3)});var d=0;n(e).scroll(function(e){var a=n(this).scrollTop();a>0&&a>d?o.addClass("hidden-nav"):o.removeClass("hidden-nav"),d=a})})}(jQuery,window,document);
---
+++ javascripts/navigation-ffd209a8.js
@@ -0,0 +1,1 @@
+!function(n,e,a){var o=n("header.navigation.auto-hide"),i=function(){o.addClass("hidden-nav")};n(a).ready(function(){var a=e.setTimeout(i,8e3);o.hover(function(){clearTimeout(a)},function(){a=e.setTimeout(i,8e3)});var d=0;n(e).scroll(function(){var e=n(this).scrollTop();e>0&&e>d?o.addClass("hidden-nav"):o.removeClass("hidden-nav"),d=e})})}(jQuery,window,document);
