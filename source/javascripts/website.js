440cf4cc4bd44ef7ab074d1ea7c2b4961368b44d
Sam Morgan <s_morgan@me.com>
2018-04-26 16:27:55 +0100

    Upgrade email partial with HubSpot tracking
    * See the documentation in _email_capture.html.haml for how this works.

--- source/javascripts/website.js
+++ source/javascripts/website.js
@@ -2,17 +2,11 @@
 
 (function($, tracking) {
   var targets = {
-    ".employers-testimonials-slider .slick-prev": "Clicked Employers Testimonials Slider",
-    ".employers-testimonials-slider .slick-next": "Clicked Employers Testimonials Slider",
-    ".graduates-slider .slick-prev": "Clicked Grads Slider",
-    ".graduates-slider .slick-next": "Clicked Grads Slider",
-    ".curriculum-slider .slick-prev": "Clicked Curriculum Slider",
-    ".curriculum-slider .slick-next": "Clicked Curriculum Slider",
     ".graduates-video": "Watched Alumni Video",
     "[data-track]": null,
   };
 
-  var properties = {"Course Type": "Offline"};
+  var properties = {"Course Type": "Onsite"};
 
   $(document).ready(function(){
     tracking.trackClicks(targets, properties);
